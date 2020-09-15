e367b286477a9bd69b45be9c516a08b581aca118
zhangxike <zhangxike@wondersgroup.com>
2020-09-15 10:53:22 +0800

    init初始化提交

---
+++ src/test/java/com/zxk/springdataelasticsearch/SpringDataElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxk.springdataelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
